c3f7a10c42c1213210183d8487792a987991961e
RyanAcosta256 <134816152+RyanAcosta256@users.noreply.github.com>
2025-03-17 19:39:16 -0400

    made the class for point, onto rectangle now

--- Point.cpp
+++ Point.cpp
@@ -3,3 +3,21 @@
 #include "Point.h"
 
 using namespace std;
+
+    Point::Point(double xCoord, double yCoord){
+        x = xCoord;
+        y = yCoord;
+    }
+
+    // Member function to calculate distance to another Point
+    double Point::distanceTo(const Point& other) const{
+        double top = other.y - y;
+        double down = other.x -x;
+        
+        return top/down;
+    }
+
+    //Member function to print the point.
+    void Point::print() const{
+        cout << x<<","<<y;
+    }
